0798918235eda4d7a9fddd0e79c7d797ba5f8ca8
viego-s <3026333612@qq.com>
2025-07-17 22:48:54 +0800

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -2,42 +2,43 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <QFileDialog>
-#include <QMessageBox>
-#include <QTextStream>
 #include <QTableWidget>
-#include <QTableWidgetItem>
-#include <QtCharts>  // 包含所有Qt Charts相关头文件
-#include <QVector>
-#include <QDebug>
+#include <QChartView>
+#include <QTextEdit>
+#include <QPushButton>
+#include <QtCharts>
 
-// 使用Qt Charts命名空间
 QT_CHARTS_USE_NAMESPACE
 
-    namespace Ui {
-    class MainWindow;
-}
-
-class MainWindow : public QMainWindow
-{
+    class MainWindow : public QMainWindow {
     Q_OBJECT
 
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
-private slots:
-    void readDataFromFile();      // 从文件读取数据
-    void saveDataToFile();        // 保存数据到文件
-    void savePlotToFile();        // 保存图片到文件
-    void clearAll();              // 清空所有内容
-
 private:
-    void plotData(const QVector<double> &col1, const QVector<double> &col2);  // 绘制曲线图
-    void calculateStats(const QVector<double> &col1, const QVector<double> &col2);  // 计算统计值
+    void setupUi();
+    void setupChart();
+    void connectSignals();
 
-    Ui::MainWindow *ui;
-    QChart *chart;                // 图表对象
+    QTableWidget *tableWidget;
+    QChartView *chartView;
+    QTextEdit *textEdit;
+    QPushButton *btnRead;
+    QPushButton *btnSaveData;
+    QPushButton *btnSavePlot;
+    QPushButton *btnClear;
+
+    QChart *chart;
+    QLineSeries *series1;
+    QLineSeries *series2;
+
+private slots:
+    void readDataFromFile();
+    void saveDataToFile();
+    void savePlotToFile();
+    void clearAll();
 };
 
 #endif // MAINWINDOW_H
